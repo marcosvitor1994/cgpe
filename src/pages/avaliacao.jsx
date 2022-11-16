a6d4794cdc3be68525d89f01e194ee4175004abb
Marcos Vitor <marcosvitor1994@gmail.com>
2022-11-16 17:29:48 -0300

    Atualizando tamanho dos dashboards

--- src/pages/avaliacao.jsx
+++ src/pages/avaliacao.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Avaliacao = () => {
   return (
     <div>
-        <iframe title="Pesquisa Telefônica 2022" width="1024" height="1060" src="https://app.powerbi.com/view?r=eyJrIjoiYWMwNDg0ZjctZDIxNy00NDZjLTgyZjQtYzE3Yzg5NGE3M2E4IiwidCI6ImExMTIwMGVkLTNhYTctNDFhMy05M2UxLTcwYWU4ZmMxZWMxYSJ9&pageName=ReportSection" frameborder="0" allowFullScreen="true"></iframe>
+        <iframe title="Pesquisa Telefônica 2022" width="100%" height="720" src="https://app.powerbi.com/view?r=eyJrIjoiYWMwNDg0ZjctZDIxNy00NDZjLTgyZjQtYzE3Yzg5NGE3M2E4IiwidCI6ImExMTIwMGVkLTNhYTctNDFhMy05M2UxLTcwYWU4ZmMxZWMxYSJ9&pageName=ReportSection" frameborder="0" allowFullScreen="true"></iframe>
     </div>
   )
 }
--- src/pages/tematicas.jsx
+++ src/pages/tematicas.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Tematicas = () => {
   return (
     <div>
-        <iframe title="Pesquisa Temática Semanal" width="1024" height="1060" src="https://app.powerbi.com/view?r=eyJrIjoiY2NjY2Q0OWItZWQ0My00MzViLWEzNTgtMjQ4ZmQzOGViOWYxIiwidCI6ImExMTIwMGVkLTNhYTctNDFhMy05M2UxLTcwYWU4ZmMxZWMxYSJ9&pageName=ReportSection5d85b9e8ddf7f8bdc2fb" frameborder="0" allowFullScreen="true"></iframe>
+        <iframe title="Pesquisa Temática Semanal" width="100%" height="720" src="https://app.powerbi.com/view?r=eyJrIjoiY2NjY2Q0OWItZWQ0My00MzViLWEzNTgtMjQ4ZmQzOGViOWYxIiwidCI6ImExMTIwMGVkLTNhYTctNDFhMy05M2UxLTcwYWU4ZmMxZWMxYSJ9&pageName=ReportSection5d85b9e8ddf7f8bdc2fb" frameborder="0" allowFullScreen="true"></iframe>
     </div>
   )
 }
